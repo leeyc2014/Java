a04dd2c7f2dfcdee76ab9940b51232c5aa43b3e4
leeyc2014 <leeyc20145@naver.com>
2025-09-23 14:04:00 +0900

    2_4, 2_5 수정

--- 자료구조ch02/train_실습2_5정수배열정렬.java
+++ 자료구조ch02/train_실습2_5정수배열정렬.java
@@ -3,6 +3,7 @@ package 자료구조ch02;
  * 2장 - 정수 배열 정렬
  */
 
+import java.util.Arrays;
 //교재 67 - 실습 2-5
 //2번 실습
 import java.util.Random;
@@ -36,31 +37,56 @@ public class train_실습2_5정수배열정렬 {
 	 * O(n) 알고리즘으로 구현 
 	 */
 	private static int[] insertData(int []data, int value) {//insert되는 실수 값이 insert될 위치를 찾아 보다 큰 값은 우측으로 이동
-		int newData[] = new int[data.length+1];
-		
-		
+		int newData[] = new int[data.length + 1];
+		int newIndex = newData.length / 2; 
+		int t = 0;
+		for(int i = 0; i < newData.length; i++) {
+			if(i == newIndex) {
+				newData[i] = value;
+			}
+			else {
+				newData[i] = data[t++];
+			}
+		}
+		return newData;
 	}
 	
 	private static void inputData(int[] data) {
 		Random rd = new Random();
 		for(int i = 0; i < data.length; i++) {
-			data[i] = rd.nextInt((60) + 1);
+			data[i] = rd.nextInt((51) + 10);
 		}
 	}
 	
 	private static void showData(String msg, int[] data) {
-		
+		System.out.print(msg + "[");
+		for(int i = 0; i < data.length; i++) {
+			System.out.print(data[i]);
+			if(i < data.length - 1) {
+				System.out.print(",");
+			}
+		}
+		System.out.println("]");
 	}
 	
 	private static void reverse(int[] data) {
-		
+		for(int i = 0; i < data.length / 2; i++) {
+			int num = data[i];
+			data[i] = data[data.length - i - 1];
+			data[data.length - i - 1] = num;
+		}
 	}
 
 	private static void sortData(int[] data) {
-		
+		Arrays.sort(data);
 	}
 	
 	private static void reverseSort(int[] data) {
-		
+		Arrays.sort(data);
+		for(int i = 0; i < data.length / 2; i++) {
+			int num = data[i];
+			data[i] = data[data.length - i - 1];
+			data[data.length - i - 1] = num;
+		}
 	}
 }
