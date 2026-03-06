99869aef635b277f9696df4235f207a52e385106
leeyc2014 <leeyc20145@naver.com>
2026-03-06 17:33:55 +0900

    8-Queen 문제

--- 자료구조ch05/train_QueenEight_구현실습과제.java
+++ 자료구조ch05/train_QueenEight_구현실습과제.java
@@ -1,4 +1,5 @@
 package 자료구조ch05;
+
 //print로 변수 값 확인하는 디버깅 노동 피하자
 //이클립스 디버깅 실습 필요 : variables tab, Expressions tab 사용하기
 //92개 해 확인 필요
@@ -40,9 +41,17 @@ class Point {
 		ix = x;
 		iy = y;
 	}
+	
+	public int getX() {
+        return ix;
+    }
+
+    public int getY() {
+        return iy;
+    }
 }
 
-class Stack4 {
+class Stack5 {
 	// --- 실행시 예외: 스택이 비어있음 ---//
 	// generic class는 Throwable을 상속받을 수 없다 - 지원하지 않는다
 	public class EmptyGenericStackException extends Exception {
@@ -66,36 +75,52 @@ class Stack4 {
 	private int top; // 스택 포인터
 
 	// --- 생성자(constructor) ---//
-	public Stack4(int capacity) {
-
+	public Stack5(int capacity) {
+		this.capacity = capacity;
+		this.data = new ArrayList<>(capacity);
+		this.top = 0;
 	}
 
 	// --- 스택에 x를 푸시 ---//
 	public boolean push(Point x) throws OverflowGenericStackException {
-
-
+		if (isFull()) {
+			throw new OverflowGenericStackException("overflow stack exception");
+		}
+		data.add(x);
+		top++;
+		return true;
 	}
 
 	// --- 스택에서 데이터를 팝(정상에 있는 데이터를 꺼냄) ---//
 	public Point pop() throws EmptyGenericStackException {
-
+		if (isEmpty()) {
+			throw new EmptyGenericStackException("empty stack exception");
+		}
+		Point p = data.remove(top - 1);
+		top--;
+		return p;
 	}
 
 	// --- 스택에서 데이터를 피크(peek, 정상에 있는 데이터를 들여다봄) ---//
 	public Point peek() throws EmptyGenericStackException {
-
+		if (isEmpty()) {
+			throw new EmptyGenericStackException("empty stack exception");
+		}
+		return data.get(top - 1);
 	}
 
 	// --- 스택을 비움 ---//
 	public void clear() {
-		top = 0;
+		data.clear();
 	}
 
 	// --- 스택에서 x를 찾아 인덱스(없으면 –1)를 반환 ---//
 	public int indexOf(Point x) {
-		for (int i = top - 1; i >= 0; i--) // 꼭대기 쪽부터 선형 검색
-			if (data.get(i).equals(x))
+		for (int i = top - 1; i >= 0; i--) {// 꼭대기 쪽부터 선형 검색
+			if (data.get(i).equals(x)) {
 				return i; // 검색 성공
+			}
+		}
 		return -1; // 검색 실패
 	}
 
@@ -120,12 +145,13 @@ class Stack4 {
 	}
 
 	// --- 스택 안의 모든 데이터를 바닥 → 꼭대기 순서로 출력 ---//
-	public void dump() throws EmptyGenericStackException{
+	public void dump() throws EmptyGenericStackException {
 		if (top <= 0)
 			throw new EmptyGenericStackException("stack:: dump - empty");
 		else {
-			for (int i = 0; i < top; i++)
+			for (int i = 0; i < top; i++) {
 				System.out.print(data.get(i) + " ");
+			}				
 			System.out.println();
 		}
 	}
@@ -133,79 +159,166 @@ class Stack4 {
 
 public class train_QueenEight_구현실습과제 {
 
-	int numberOfSolutions = 0;
-	int count = 0;//퀸 배치 갯수
-	int ix = 0, iy = 0;// 행 ix, 열 iy
-	Stack5 st = new Stack5(100); //100개를 저장할 수 있는 스택을 만들고
-	Point5 p = new Point5(ix, iy);//현 위치를 객체로 만들고
-	d[ix][iy] = 1;//현 위치에 queen을 넣었다는 표시를 하고
-	count++;
-	st.push(p);// 스택에 현 위치 객체를 push
-	ix++;//ix는 행별로 퀸 배치되는 것을 말한다.
-	iy = 0;//다음 행으로 이동하면 열은 0부터 시작
-	while (true) {
-		if (st.isEmpty() && ix == 8) //ix가 8이면 8개 배치 완료, stack이 empty가 아니면 다른 해를 구한다 
-			break;
-		if ((iy = nextMove(d, ix, iy))== -1) {//다음 이동할 열을 iy로 주는데 -1이면 더이상 이동할 열이 없음을 나타냄
+	static int numberOfSolutions = 0;
 
+	public static void EightQueen(int[][] d) {
+		int count = 0;// 퀸 배치 갯수
+		int ix = 0, iy = 0;// 행 ix, 열 iy
+		Stack5 st = new Stack5(100); // 100개를 저장할 수 있는 스택을 만들고
+		try {
+			while (true) {
+				iy = nextMove(d, ix, iy);
+				if (iy == -1) {
+					if (st.isEmpty()) {
+						break;
+					}						
+					Point p = st.pop();
+					ix = p.getX();
+					iy = p.getY();
+
+					d[ix][iy] = 0;
+					count--;
+					iy++;
+					continue;
+				}
+				d[ix][iy] = 1;
+				count++;
+
+				st.push(new Point(ix, iy));
+				if (count == 8) {
+					numberOfSolutions++;
+					showQueens(d);
+
+					Point p = st.pop();
+					ix = p.getX();
+					iy = p.getY();
+
+					d[ix][iy] = 0;
+					count--;
+					iy++;
+				} 
+				else {
+					ix++;
+					iy = 0;
+				}
+			}
+			System.out.println("총 해의 개수: " + numberOfSolutions);
+		} 
+		catch (Exception e) {
+			e.printStackTrace();
 		}
-
-		if (count == 8) { //8개를 모두 배치하면
-
-		}
-
 	}
 
-}
-
-
-public static boolean checkRow(int[][] d, int crow) { //배열 d에서 행 crow에 퀸을 배치할 수 있는지 조사
-
-}
-
-public static boolean checkCol(int[][] d, int ccol) {//배열 d에서 열 ccol에 퀸을 배치할 수 있는지 조사
-
-}
-//배열 d에서 행 cx, 열 cy에 퀸을 남서, 북동 대각선으로 배치할 수 있는지 조사
-public static boolean checkDiagSW(int[][] d, int cx, int cy) { // x++, y-- or x--, y++ where 0<= x,y <= 7
-
-}
-
-//배열 d에서 행 cx, 열 cy에 퀸을 남동, 북서 대각선으로 배치할 수 있는지 조사
-public static boolean checkDiagSE(int[][] d, int cx, int cy) {// x++, y++ or x--, y--
-
-}
-//배열 d에서 (x,y)에 퀸을 배치할 수 있는지  조사
-public static boolean checkMove(int[][] d, int x, int y) {// (x,y)로 이동 가능한지를 check
-	if(checkDiagSE(d, x, y) == true || checkDiagSW(d, x, y) == true || checkCol(d, y) == true || checkRow(d, x) == true) {
+	public static boolean checkRow(int[][] d, int crow) { // 배열 d에서 행 crow에 퀸을 배치할 수 있는지 조사
+		for(int i = 0; i < d.length; i++) {
+			if(d[crow][i] == 1) {
+				return true;
+			}
+		}
 		return false;
 	}
-	else {
-		return true;
-	}	
-}
+
+	public static boolean checkCol(int[][] d, int ccol) {// 배열 d에서 열 ccol에 퀸을 배치할 수 있는지 조사
+		for(int i = 0; i < d.length; i++) {
+			if(d[i][ccol] == 1) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+//배열 d에서 행 cx, 열 cy에 퀸을 남서, 북동 대각선으로 배치할 수 있는지 조사
+	public static boolean checkDiagSW(int[][] d, int cx, int cy) { // x++, y-- or x--, y++ where 0<= x,y <= 7
+		int x = cx;
+        int y = cy;
+
+        while (x >= 0 && y < 8) {
+            if (d[x][y] == 1) {
+            	return true;
+            }                
+            x--;
+            y++;
+        }
+        x = cx;
+        y = cy;
+
+        while (x < 8 && y >= 0) {
+            if (d[x][y] == 1) {
+            	return true;
+            }                
+            x++;
+            y--;
+        }
+        return false;
+	}
+
+//배열 d에서 행 cx, 열 cy에 퀸을 남동, 북서 대각선으로 배치할 수 있는지 조사
+	public static boolean checkDiagSE(int[][] d, int cx, int cy) {// x++, y++ or x--, y--
+		int x = cx;
+        int y = cy;
+
+        while (x >= 0 && y >= 0) {
+            if (d[x][y] == 1) {
+            	return true;
+            }              
+            x--;
+            y--;
+        }
+        x = cx;
+        y = cy;
+
+        while (x < 8 && y < 8) {
+            if (d[x][y] == 1) {
+            	return true;
+            }                
+            x++;
+            y++;
+        }
+        return false;
+	}
+
+//배열 d에서 (x,y)에 퀸을 배치할 수 있는지  조사
+	public static boolean checkMove(int[][] d, int x, int y) {// (x,y)로 이동 가능한지를 check
+		if (checkDiagSE(d, x, y) == true || checkDiagSW(d, x, y) == true || checkCol(d, y) == true
+				|| checkRow(d, x) == true) {
+			return false;
+		} else {
+			return true;
+		}
+	}
+
 //배열 d에서 현재 위치(row,col)에 대하여 다음에 이동할 위치 nextCol을 반환, 이동이 가능하지 않으면 -1를 리턴
-public static int nextMove(int[][] d, int row, int col) {// 현재 row, col에 대하여 이동할 col을 return
-	for(int i = col; i < d[0].length; i++) {
-		if(checkMove(d, row, i) == true) {
-			return i;
+	public static int nextMove(int[][] d, int row, int col) {// 현재 row, col에 대하여 이동할 col을 return
+		for (int i = col; i < d[0].length; i++) {
+			if (checkMove(d, row, i) == true) {
+				return i;
+			}
 		}
 		return -1;
 	}
-}
 
-static void showQueens(int[][] data) {// 배열 출력
+	static void showQueens(int[][] data) {// 배열 출력
+		System.out.println("해 " + (numberOfSolutions));
+        for (int i = 0; i < 8; i++) {
+            for (int j = 0; j < 8; j++) {
+                if (data[i][j] == 1) {
+                	System.out.print("Q ");
+                }                    
+                else {
+                	System.out.print(". ");
+                }                    
+            }
+            System.out.println();
+        }
+        System.out.println();
+	}
 
-}
+	public static void main(String[] args) {
+		int[][] data = new int[8][8];
+		for (int i = 0; i < data.length; i++)
+			for (int j = 0; j < data[0].length; j++)
+				data[i][j] = 0;
 
-public static void main(String[] args) {
-	int row = 8, col = 8;
-	int[][] data = new int[8][8];
-	for (int i = 0; i < data.length; i++)
-		for (int j = 0; j < data[0].length; j++)
-			data[i][j] = 0;
-
-	EightQueen(data);
-
-}
+		EightQueen(data);
+	}
 }
